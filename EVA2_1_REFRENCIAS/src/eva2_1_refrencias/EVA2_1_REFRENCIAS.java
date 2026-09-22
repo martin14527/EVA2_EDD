0647bf6bf1d47b0da913da1050ffb912e417a539
martin14527 <guillermovallejoloya@gmail.com>
2026-09-21 21:30:43 -0600

    Update EVA2_1_REFRENCIAS.java

--- EVA2_1_REFRENCIAS/src/eva2_1_refrencias/EVA2_1_REFRENCIAS.java
+++ EVA2_1_REFRENCIAS/src/eva2_1_refrencias/EVA2_1_REFRENCIAS.java
@@ -1,39 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package eva2_1_refrencias;
 
-/**
- *
- * @author anoni
- */
 public class EVA2_1_REFRENCIAS {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+
         ejemplo inicio;
         inicio = new ejemplo();
-        inicio.valor =100;
-        System.out.println("inicio " + valor);
-         System.out.println("inicio " + inicio.otro);
-         System.out.println("inicio otro " + inicio.otro);
-         //crear una lista
-         inicio.otro = new ejemplo();
-         inicio.otro.valor = 200;
-         inicio.otro.otro.valor = 300 //encadenas listas de forma artesanal.
-         inicio.otro.otro.otro.valor = 400;
-         ejemplo sig = inicio;
-         while(sig ! = null){
-     System.out.println(sig.otro + " k");
-    
-    };
-    } 
-    class ejemplo {
-    int valor;
-    ejemplo otro;
-    
+
+        inicio.valor = 100;
+
+        System.out.println("inicio " + inicio.valor);
+        System.out.println("inicio otro " + inicio.otro);
+
+        // Crear una lista
+        inicio.otro = new ejemplo();
+        inicio.otro.valor = 200;
+
+        inicio.otro.otro = new ejemplo();
+        inicio.otro.otro.valor = 300;
+
+        inicio.otro.otro.otro = new ejemplo();
+        inicio.otro.otro.otro.valor = 400;
+
+        // Recorrer la lista
+        ejemplo sig = inicio;
+
+        while (sig != null) {
+            System.out.println(sig.valor + " k");
+            sig = sig.otro;
+        }
+    }
+
+    static class ejemplo {
+        int valor;
+        ejemplo otro;
     }
 }
